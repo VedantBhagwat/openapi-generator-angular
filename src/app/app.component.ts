9f80a7e7e6ad3a67db793bd34db405e691d71c67
Vedant Bhagwat <vedant143bhagwat@gmail.com>
2024-11-28 21:18:24 +0530

    Add prettier library for better formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { UserApiFactory } from '../lib/axios-client';
+import {Component} from '@angular/core'
+import {RouterOutlet} from '@angular/router'
+import {UserApiFactory} from '../lib/axios-client'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'openapi-generator-angular';
-  userApi = UserApiFactory();
+  title = 'openapi-generator-angular'
+  userApi = UserApiFactory()
 
   constructor() {
     this.userApi
-      .loginUser({ username: 'admin', password: 'password' })
+      .loginUser({username: 'admin', password: 'password'})
       .then((response) => {
-        console.log('loginUser', response);
+        console.log('loginUser', response)
       })
       .catch((error) => {
-        console.error(error);
-      });
+        console.error(error)
+      })
   }
 }
